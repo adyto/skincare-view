e8c54efb80a52ba63d9a4900cd1d4f456297f32b
adyto <adiyulianto61@gmail.com>
2022-06-05 14:31:42 +0700

    adding section normal-skin

--- pages/bodycare/bodycare-tips.tsx
+++ pages/bodycare/bodycare-tips.tsx
@@ -19,7 +19,7 @@ const BodyCareTips: NextPage = () => {
       <Navbar activeMenu="bodycare" />
 
       {/* BodyCare-Tips */}
-      <section id="solusiStep" className='solusi pt-30 pb-20'>
+      <section className='body-care-tips pt-30 pb-20'>
         <div className='container-fluid' data-aos="fade-up">
           <div className="d-flex justify-content-center">
             <div className="position-relative" data-aos="zoom-in">
@@ -51,19 +51,25 @@ const BodyCareTips: NextPage = () => {
                 </Link>
               </div>
               <div className="column-card mb-1">
-                <a className='btn-md text-white text-md' href='/bodycare/bodycare-tips/#'>
-                  Kulit Normal
-                </a>
+                <Link href='/bodycare/bodycare-tips/#normal-skin'>
+                  <a className='btn-md text-white text-md' role='button'>
+                    Kulit Normal
+                  </a>
+                </Link>
               </div>
               <div className="column-card mb-1">
-                <a className='btn-md text-white text-md' href='/bodycare/bodycare-tips/#'>
-                  Kulit Sensitif
-                </a>
+                <Link href='/bodycare/bodycare-tips/#sensitive-skin'>
+                  <a className='btn-md text-white text-md' role='button'>
+                    Kulit Sensitif
+                  </a>
+                </Link>
               </div>
               <div className="column-card mb-1">
-                <a className='btn-md text-white text-md' href='/bodycare/bodycare-tips/#'>
-                  Kulit Kombinasi
-                </a>
+                <Link href='/bodycare/bodycare-tips/#combination-skin'>
+                  <a className='btn-md text-white text-md' role='button'>
+                    Kulit Kombinasi
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -165,7 +171,7 @@ const BodyCareTips: NextPage = () => {
       </section>
 
       {/* Oily Skin */}
-      <section id="oily-skin" className='dry-skin pt-3 pb-1'>
+      <section id="oily-skin" className='oily-skin pt-3 pb-1'>
         <div className='container-fluid container-xxl' data-aos="fade-up">
           <div className='d-flex justify-content-center'>
             <div className='position-relative'>
@@ -246,6 +252,59 @@ const BodyCareTips: NextPage = () => {
         </div>
       </section>
 
+      {/* Normal Skin */}
+      <section id="normal-skin" className='normal-skin pt-3 pb-1'>
+        <div className='container-fluid container-xxl' data-aos="fade-up">
+          <div className='d-flex justify-content-center'>
+            <div className='position-relative'>
+              <Image src='/icon/lamp.png' width={100} height={100} />
+            </div>
+          </div>
+          <h2 className='text-4xl fw-bold color-palette-1 text-center mb-2'>
+            KULIT NORMAL
+          </h2>
+          <div className='row gap-lg-0 gap-5 mt-4'>
+            <div className='col-lg-6 col-12 my-auto' data-aos="zoom-in">
+              <div className='d-flex justify-content-lg-end me-lg-5'>
+                <Image src='/img/normal-skin.png' width={400} height={400} />
+              </div>
+            </div>
+            <div className='col-lg-6 col-12 d-lg-block'>
+              <div className='d-flex justify-content-lg-start'>
+                <div className='mt-4 fs-5'>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Kulit normal umumnya tampak merona, tidak kusam, berminyak, atau kering, serta tidak memiliki banyak kerutan. Tipe kulit normal juga lebih mudah dirawat daripada jenis kulit lainnya.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Jika kulit Anda termasuk tipe kulit normal, Anda hanya perlu rutin menggunakan pelembap berbahan dasar air yang bersifat ringan dan tidak lengket untuk menjaga kelembapan alami kulit.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='d-flex justify-content-center mt-2'>
+            <p className='color-palette-1'>*Rekomendasi perawatan kulit badan dapat menggunakan produk seperti <span className='fw-semibold'>Body Scrub, Body Shower</span> dan <span className="fw-semibold">Body Lotion.</span></p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
