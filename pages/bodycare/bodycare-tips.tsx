2ed32ab6fcb54587b7a92f4944feabbe165e5e63
adyto <adiyulianto61@gmail.com>
2022-06-05 14:05:34 +0700

    adding section oily-skin

--- pages/bodycare/bodycare-tips.tsx
+++ pages/bodycare/bodycare-tips.tsx
@@ -44,9 +44,11 @@ const BodyCareTips: NextPage = () => {
                 </Link>
               </div>
               <div className="column-card mb-1">
-                <a className='btn-md text-white text-md' href='/bodycare/bodycare-tips/#' role="button">
-                  Kulit Berminyak
-                </a>
+                <Link href="/bodycare/bodycare-tips/#oily-skin">
+                  <a className='btn-md text-white text-md' role="button">
+                    Kulit Berminyak
+                  </a>
+                </Link>
               </div>
               <div className="column-card mb-1">
                 <a className='btn-md text-white text-md' href='/bodycare/bodycare-tips/#'>
@@ -159,15 +161,91 @@ const BodyCareTips: NextPage = () => {
           <div className='d-flex justify-content-center mt-2'>
             <p className='color-palette-1'>*Rekomendasi perawatan kulit badan dapat menggunakan produk seperti <span className='fw-semibold'>Body Scrub, Body Shower</span> dan <span className="fw-semibold">Body Lotion.</span></p>
           </div>
-          <div className='btn-shampoo col-9 col-sm-7 col-md-6 col-lg-5 col-xl-4 col-xxl-3  mx-auto'>
-            <div className="d-sm-flex text-center justify-content-center ">
-              <a className='btn-md text-white' role="button" >
-                Pesan Sekarang !
-              </a>
+        </div>
+      </section>
+
+      {/* Oily Skin */}
+      <section id="oily-skin" className='dry-skin pt-3 pb-1'>
+        <div className='container-fluid container-xxl' data-aos="fade-up">
+          <div className='d-flex justify-content-center'>
+            <div className='position-relative'>
+              <Image src='/icon/lamp.png' width={100} height={100} />
             </div>
+          </div>
+          <h2 className='text-4xl fw-bold color-palette-1 text-center mb-2'>
+            KULIT BERMINYAK
+          </h2>
+          <p className="color-palette-1 fw-normal fs-5 mx-2">
+            <span className="d-block mb-2">Kulit berminyak biasanya terlihat kusam atau berkilau dan memiliki pori-pori besar. Tipe kulit ini juga lebih mudah berkomedo dan muncul jerawat. Kulit bisa lebih berminyak ketika kelenjar minyak (kelenjar sebasea) di kulit menghasilkan terlalu banyak sebum.</span>
+            <span className="d-block mb-2">Ada beberapa faktor yang dapat membuat seseorang lebih rentan mengalami kulit berminyak, di antaranya faktor genetik, stres, udara lembap, dan perubahan hormon, misalnya pada remaja yang memasuki masa pubertas.</span ><span className="d-block mb-2">Kulit berminyak dapat dirawat dengan beberapa cara berikut ini:</span>
+          </p>
+          <div className='row gap-lg-0 gap-5 mt-4'>
+            <div className='col-lg-6 col-12 my-auto' data-aos="zoom-in">
+              <div className='d-flex justify-content-lg-end me-lg-5'>
+                <Image src='/img/oily-skin.png' width={400} height={400} />
+              </div>
+            </div>
+            <div className='col-lg-6 col-12 d-lg-block'>
+              <div className='d-flex justify-content-lg-start'>
+                <div className='mt-2 fs-5'>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Menggunakan produk perawatan berlabel bebas minyak atau noncomedogenic yang tidak menyumbat pori-pori</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Mencuci wajah 2 kali sehari, khususnya saat kulit wajah berkeringat</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Menghindari memencet jerawat karena dapat meninggalkan bekas luka yang sulit hilang</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-1">
+                      <div className="position-relative">
+                        <Image src='/icon/logo-ceklis.png' width={30} height={30} layout="fixed" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="position-relative">
+                        <p className="color-palette-1">Menggunakan produk perawatan kulit berbahan dasar air agar tidak membuat kulit semakin berminyak dan mencegah munculnya jerawat</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='d-flex justify-content-center mt-2'>
+            <p className='color-palette-1'>*Rekomendasi perawatan kulit badan dapat menggunakan produk seperti <span className='fw-semibold'>Body Scrub, Body Shower</span> dan <span className="fw-semibold">Body Lotion.</span></p>
           </div>
         </div>
       </section>
+
     </>
   )
 }
